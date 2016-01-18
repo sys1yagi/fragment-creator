60abd3c67b15fbb15e9437a05a426b87252ecfde
sys1yagi <sylc.yagi@gmail.com>
2016-01-19 00:56:30 +0900

    Remove unused code.

--- library/src/main/java/com/sys1yagi/fragmentcreator/FragmentCreator.java
+++ library/src/main/java/com/sys1yagi/fragmentcreator/FragmentCreator.java
@@ -3,8 +3,6 @@ package com.sys1yagi.fragmentcreator;
 import com.sys1yagi.fragmentcreator.exception.InvalidParameterException;
 import com.sys1yagi.fragmentcreator.exception.UnsupportedTypeException;
 
-import java.lang.reflect.Field;
-
 public abstract class FragmentCreator {
 
     public static void checkRequire(Object param, String paramName) {
@@ -18,31 +16,5 @@ public abstract class FragmentCreator {
             //TODO rich message
             throw new InvalidParameterException(param.getClass().getName() + " is invalid.");
         }
-    }
-
-    public static <T> void set(T instance, String fieldName, Object value) {
-        try {
-            Class<?> clazz = instance.getClass();
-            Field field = clazz.getField(fieldName);
-            field.setAccessible(true);
-            field.set(instance, value);
-            field.setAccessible(false);
-        } catch (Exception e) {
-            //no op
-        }
-    }
-
-    public static <T> Object get(T instance, String fieldName) {
-        try {
-            Class<?> clazz = instance.getClass();
-            Field field = clazz.getField(fieldName);
-            field.setAccessible(true);
-            Object value = field.get(instance);
-            field.setAccessible(false);
-            return value;
-        } catch (Exception e) {
-            //no op
-        }
-        return null;
     }
 }
