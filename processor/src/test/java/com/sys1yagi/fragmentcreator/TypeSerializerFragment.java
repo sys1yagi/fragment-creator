32101f362e679f49f18978c5eaa66fca452e1855
Toshihiro Yagi <sylc.yagi@gmail.com>
2019-01-05 12:17:33 +0900

    try fix test...

---
+++ processor/src/test/java/com/sys1yagi/fragmentcreator/TypeSerializerFragment.java
@@ -0,0 +1,27 @@
+package com.sys1yagi.fragmentcreator;
+
+import com.sys1yagi.fragmentcreator.ArgsSerializer;
+import com.sys1yagi.fragmentcreator.annotation.Args;
+import com.sys1yagi.fragmentcreator.annotation.FragmentCreator;
+import com.sys1yagi.fragmentcreator.annotation.Serializer;
+
+@FragmentCreator
+public class MainFragment extends Fragment {
+
+    @Args
+    @Serializer(to = String.class, serializer = StringSerializer.class)
+    int id;
+
+    static class StringSerializer implements ArgsSerializer<Integer, String> {
+
+        @Override
+        public String serialize(Integer integer) {
+            return integer.toString();
+        }
+
+        @Override
+        public Integer deserialize(String s) {
+            return Integer.parseInt(s);
+        }
+    }
+}
